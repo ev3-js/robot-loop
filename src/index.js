6dd34a067282ecccbceae2fa1d1e74568c7e8a09
Daniel <danleavitt0@gmail.com>
2016-03-31 11:17:42 -0700

    add motor

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import compose from '@f/compose'
 import flatten from '@f/flatten-gen'
 import {composable} from 'yoco'
 import cycle, {out} from 'cycle-shell'
-var {robot, move, sleep, read, runAction} = require('ev3-client')
+var {robot, move, motor, sleep, read, runAction} = require('ev3-client')
 
 module.exports = {
   loop: robotLoop,
   move,
+  motor,
   sleep,
   read,
   out
