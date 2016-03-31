f2670361d51e43f32df34075df22e2b22e64ab71
Daniel <danleavitt0@gmail.com>
2016-03-30 19:42:50 -0700

    fix loop

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill'
+import compose from '@f/compose'
+import flatten from '@f/flatten-gen'
+import {composable} from 'yoco'
+import cycle, {out} from 'cycle-shell'
+var {robot, move, sleep, read, runAction} = require('ev3-client')
+
+module.exports = {
+  loop: robotLoop,
+  move,
+  sleep,
+  read,
+  out
+}
+
+function robotLoop (main, address) {
+  var run = robot(address)
+  cycle(composable([run.mw])(compose(runAction, flatten(main))))
+  return run
+}
