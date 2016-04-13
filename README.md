7bef95c8eebb6a1169b62a4d4176d19b499a4cec
Daniel <danleavitt0@gmail.com>
2016-04-12 18:52:54 -0700

    add comments

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # robot-loop
+
 Set up cycle shell loop for ev3-client
+
+## Installation
+
+    npm install robot-loop
+
+## Usage
+
+```js
+var {loop, move, motor, out, sleep, read} = require('robot-loop')
+loop(main, '192.168.1.100')
+
+var steer = move('b', 'c')
+
+function * main () {
+  yield steer.rotations(1, 60, 0)
+}
+```
+
+## API
+
+### 
+
+Imports
+
+### index
+
+Exports
+
+### robotLoop
+
+Connect the main function to the ev3-client run middleware and use it to create a cycle shell loop
+
+**Parameters**
+
+-   `main` **fn** generator function yielding ev3-client functions
+-   `address` **[string](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String)** string ip address of the robotLoop
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+/**
+ * Imports
+ */
 import 'babel-polyfill'
 import compose from '@f/compose'
 import flatten from '@f/flatten-gen'
 import {composable} from 'yoco'
 import cycle, {out} from 'cycle-shell'
+
+/**
+ * destructure ev3-client methods
+ */
 var {robot, move, motor, sleep, read, runAction} = require('ev3-client')
 
+/**
+ * Exports
+ * @type {Object}
+ */
 module.exports = {
   loop: robotLoop,
   move,
@@ -14,10 +25,14 @@ module.exports = {
   out
 }
 
+/**
+ * Connect the main function to the ev3-client run middleware and use it to create a cycle shell loop
+ * @param  {fn} main           generator function yielding ev3-client functions
+ * @param  {string} address    string ip address of the robotLoop
+ */
 function robotLoop (main, address) {
   var run = robot(address)
   cycle(composable([run.mw])(compose(runAction, flatten(main))), {
     title: 'ev3'
   })
-  return run
 }
